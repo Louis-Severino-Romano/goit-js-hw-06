b49a5945453cfe8680b012fc799e72c8a11937ef
Louis-Severino-Romano <louis.romano@ctu.edu.ph>
2024-03-19 18:58:23 +0800

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let addBtn=document.querySelector('[data-action="increment"]');
+let minusBtn=document.querySelector('[data-action="decrement"]');
+let initialValue=0;
+
+let value=document.getElementById('value');
+value.innerText=initialValue;
+addBtn.onclick = function(){
+    initialValue += 1;
+    value.innerText=initialValue;
+}
+
+minusBtn.onclick = function(){
+    if(initialValue>0){
+        initialValue -=1;
+        value.innerText=initialValue;
+    }
+
+}
